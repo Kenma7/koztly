a75cbbaa1878dfca98faf095cac159d5023b4da2
Moebius09 <delianurilmi@gmail.com>
2025-09-26 17:19:21 +0700

    push routes admin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Route ke admin
+Route::get('/admin', function () {
+    return ('hello'); // tampilkan dashboard
+});
+
