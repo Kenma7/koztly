f1ef0a4dfe7cdc2500ae2a9a82b355122bf56581
isaluwu <faishalmuhammad233@gmail.com>
2025-09-27 03:31:34 +0700

    'push-model-kosan.php'

---
+++ app/Models/Kosan.php
@@ -0,0 +1,23 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kosan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kosan';
+    protected $primaryKey = 'id_kos';
+    protected $fillable = [
+        'nama_kos',
+        'lokasi_kos',
+        'harga',
+        'jumlah_kamar',
+        'fasilitas',
+        'kategori',
+        'status',
+        'gambar_kos',
+        'no_rek',
+    ];
+}
