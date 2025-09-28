8ea3bc7d0150d1bea5432a72bbb38d0ccb0da6a4
Moebius09 <delianurilmi@gmail.com>
2025-09-28 12:25:11 +0700

    add column lama_sewa di table booking kos (migrations)

---
+++ database/migrations/2025_09_28_052325_add_lama_sewa_to_booking_kos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('booking_kos', function (Blueprint $table) {
+            //
+             $table->integer('lama_sewa')->default(1)->after('harga');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('booking_kos', function (Blueprint $table) {
+            //
+            $table->dropColumn('lama_sewa');
+        });
+    }
+};
