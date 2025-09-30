8bb0728d576ce500b7e66a60bf63adff156dfced
isaluwu <faishalmuhammad233@gmail.com>
2025-10-01 00:46:56 +0700

    minor change

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,8 @@
 import './bootstrap';
-<<<<<<< HEAD
 import './sidebar.js';
-=======
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
->>>>>>> a1e647272ea359ca33cdfeed7ee77995b77a4294
